e926494ada1afe122f8e6e4ae6219b3475a4dcfc
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-10-26 21:45:55 +0330

    gforth: add example on conditional exit from word definition

--- gforth/word-definition.fs
+++ gforth/word-definition.fs
@@ -21,3 +21,15 @@
     squared squared ;
 
 3 4th-power . cr
+
+\ If the TOS is 3 exit the definition
+: exit-if-3 ( n -- )
+    dup 3 = if
+        ." TOS is 3, exiting ..." cr
+        drop exit
+    then
+    ." This will not be executed if TOS is 3" cr
+    ;
+
+4 exit-if-3
+3 exit-if-3
