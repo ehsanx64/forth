8b1cc0a53b4a35cc3f8b8404ea6f67474676ff19
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-11-12 23:53:07 +0330

    gforth: add heap memory allocation to memory.fs

--- gforth/memory.fs
+++ gforth/memory.fs
@@ -42,19 +42,25 @@ v2 20 cells dump
 
 : vsum-1 ( addr u -- n )
     0 -rot 1+ 0 ?do
-        cr dup i cells + @ rot + swap 
-    loop ;
+        dup i cells + @ rot + swap 
+    loop drop ;
 
 v2 4 vsum-1
-cr .s cr
+cr ." 4 vsum-1 -> " . cr
 
 \ Another method to allocate memory on dictionary is using following un-orthodox
 \ method
 
 \ here address was retrieved before using allot. allot alters the here address
-." Allocating 2 cells on the dictionary ..."
+cr ." Allocating 2 cells on the dictionary ..."
 here 2 cells allot cr
 ." Old here: " . cr
 ." New here: " here . cr
 
-
+\ allocate and free can be used to allocate memory on the heap instead of the
+\ dictionary
+cr ." Allocating space on the heap ..." cr
+10 cells allocate throw .s cr
+20 cells allocate throw .s cr
+swap free throw free throw
+.s cr
